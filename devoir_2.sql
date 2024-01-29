bffac3d9a45652f3d1e24a7a04d0574fed83353f
emmouhal01 <mohammed.mouhallil@gmail.com>
2024-01-29 15:43:44 -0400

    exo3

--- devoir_2.sql
+++ devoir_2.sql
@@ -19,29 +19,4 @@ SELECT title, price
 FROM titles
 WHERE title_id LIKE 'SU%' OR title_id LIKE 'BU%';
 
--- question 4
-
-SELECT title, price
-FROM titles
-WHERE title_id NOT LIKE 'SU%' AND title_id NOT LIKE 'BU%';
-
--- question 5
-
-SELECT title, price
-FROM titles
-WHERE title NOT RLIKE '^[SB]' 
-    AND title LIKE '_o%';
-
--- question 6
-
-SELECT title, price
-FROM titles
-WHERE title NOT RLIKE '^[SB]' 
-    AND title LIKE '__f%';
-
--- question 7
-
-SELECT title, price
-FROM titles
-WHERE title RLIKE '^[A-J]';
 
